ea8a1104c18af586bd76d35c2c4590d31058d49c
Mahmoud Saber <abusaber555@gmail.com>
2025-03-07 00:10:49 +0200

    update locust.py

--- locustfile.py
+++ locustfile.py
@@ -1,22 +1,27 @@
 from locust import HttpUser, task, between
+import json
 
 class FlaskAppUser(HttpUser):
-    wait_time = between(1, 3)  
+    wait_time = between(1, 3)  # Wait time between requests
 
     @task
-    def test_homepage(self):
-        self.client.get("/")  
-
+    def test_home_page(self):
+        self.client.get("/")
+    
     @task
-    def test_performance_data(self):
-        self.client.get("/performance-data")  
-
+    def test_app_performance(self):
+        self.client.get("/app-performance")
+    
     @task
-    def test_api_post(self):
-        headers = {"Content-Type": "application/json"}
-        payload = {"input": "test_data"}
-        self.client.post("/api", json=payload, headers=headers)  
-
+    def test_user_activity(self):
+        self.client.get("/user-activity")
+    
     @task
-    def test_health_check(self):
-        self.client.get("/health")  
+    def test_request_analysis(self):
+        data = {"request_input": "https://jsonplaceholder.typicode.com/posts/1", "method": "GET"}
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self.client.post("/request-analysis", data=data, headers=headers)
+
+if __name__ == "__main__":
+    import os
+    os.system("locust -f locust_test.py --headless -u 10 -r 2 --run-time 1m --csv=locust_results")
